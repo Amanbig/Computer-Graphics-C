54411e7fbf9cd0864c97e1dd7e677fd164ceba5c
Amanbig <93067432+Amanbig@users.noreply.github.com>
2023-10-18 20:01:17 +0530

    This is circle drawing algorithm implementation

---
+++ circle.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <graphics.h>
+
+using namespace std;
+
+void plotpoints(int, int, int, int);
+
+void circles(int xcenter, int ycenter, int r)
+{
+    int p = 1 - r;
+    int x = 0;
+    int y = r;
+
+    plotpoints(xcenter, ycenter, x, y);
+    
+    while (x < y)
+    {
+        x++;
+        if (p < 0)
+        {
+            p += 2 * x + 1;
+        }
+        else
+        {
+            y--;
+            p += 2 * x + 1 - 2 * y;
+        }
+        plotpoints(xcenter, ycenter, x, y);
+        delay(100);
+    }
+}
+
+void plotpoints(int xcenter, int ycenter, int x, int y)
+{
+    putpixel(xcenter + x, ycenter + y, WHITE);
+    cout << "(" << xcenter + x << "," << ycenter + y << ")" << endl;
+    putpixel(xcenter + x, ycenter - y, WHITE);
+    cout << "(" << xcenter + x << "," << ycenter - y << ")" << endl;
+    putpixel(xcenter - x, ycenter + y, WHITE);
+    cout << "(" << xcenter - x << "," << ycenter + y << ")" << endl;
+    putpixel(xcenter - x, ycenter - y, WHITE);
+    cout << "(" << xcenter - x << "," << ycenter - y << ")" << endl;
+    putpixel(xcenter + y, ycenter + x, WHITE);
+    cout << "(" << xcenter + y << "," << ycenter + x << ")" << endl;
+    putpixel(xcenter + y, ycenter - x, WHITE);
+    cout << "(" << xcenter + y << "," << ycenter - x << ")" << endl;
+    putpixel(xcenter - y, ycenter + x, WHITE);
+    cout << "(" << xcenter - y << "," << ycenter + x << ")" << endl;
+    putpixel(xcenter - y, ycenter - x, WHITE);
+    cout << "(" << xcenter - y << "," << ycenter - x << ")" << endl;
+}
+
+int main()
+{
+    int gDriver = DETECT, gMode;
+    initgraph(&gDriver, &gMode, NULL);
+    int a, b, c;
+    cout << "Enter center of circle\n";
+    cin >> a >> b;
+    cout << "Enter radius of circle\n";
+    cin >> c;
+    circles(a, b, c);
+    getch();
+    closegraph();
+    return 0;
+}
